8dab5d4881aef91f4bcb892aa2021397eaedd8d6
Josh Frazier <jofrazier@my365.bellevue.edu>
2022-06-26 15:17:23 -0400

    delete.py assignement 6.3

--- module_6/pytech_delete.py
+++ module_6/pytech_delete.py
@@ -9,21 +9,19 @@ client = MongoClient(url)
 
 #create a variable named db
 db = client.pytech
+
 #defining students and docs to efficiently search with find() function
+
 students = db.students
+db.students.deleteOne({"student_id":"1010"})
 docs = db.students.find({})
+
 print("\n DISPLAYING STUDENTS DOCUMENTS FROM find() QUERY --")
 
 for doc in docs:
+    
     print("Students ID:" + doc["student_id"] + "\n First Name:" + doc["first_name"] + "\n Last Name:" + doc["last_name"] + "\n")
 
-db.students.update_one({"student_id":"1007"}, {"$set":{"last_name":"Thunderbolt"}})
-
-Frazier=students.find_one({"student_id":"1007"})
-
-print("Student ID:"+Frazier["student_id"]+"\n First Name:"+Frazier["first_name"]+"\nLast Name:"+Frazier["last_name"]+"\n")
 #eop
 input("\n\n End of Program, press any key to exit...")
-
-
 
